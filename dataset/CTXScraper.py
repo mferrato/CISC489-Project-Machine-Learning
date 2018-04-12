419909e3a4e96e1603ac343d2337728681129cd4
efwright <efwright@udel.edu>
2018-04-12 15:42:27 -0400

    Made CTXScraper more user-friendly. Closed browser sooner and added download progress.

--- dataset/CTXScraper.py
+++ dataset/CTXScraper.py
@@ -2,66 +2,91 @@ import csv
 import sys
 import urllib
 import cStringIO
+import time
 from selenium import webdriver # external download
 
 # CTXScraper.py
 # Eric Wright, Mauricio Ferrato
 
-# Used to download CTX images from https://viewer.mars.asu.edu/viewer/ctx#T=0
+# Used to download CTX images from https://viewer.mars.asu.edu/viewer/ctx
 # These images are TIFF format, which is the highest quality image we can get
 
 # Takes as input the name of a CSV file
 # This file should have a column named "CTX image" that contains the CTX image name
 
-# Get CSV Filename from args
-csvfile=open(sys.argv[1], 'rb')
 
-# Read in all lines from CSV file (seperated by comma)
-csvreader = csv.DictReader(csvfile, delimiter=',')
+def main():
+	# Get CSV Filename from args
+	csvfile=open(sys.argv[1], 'rb')
 
-# For each row in CSV file
-for row in csvreader:
-	ctx=row['CTX image'] # CTX name of image
-	print(ctx)
-	url = "https://viewer.mars.asu.edu/viewer/ctx#P=" + ctx + "&T=2" # Database URL based on CTX
+	# Read in all lines from CSV file (seperated by comma)
+	csvreader = csv.DictReader(csvfile, delimiter=',')
+	# For each row in CSV file
+	for row in csvreader:
+		ctx=row['CTX image'] # CTX name of image
+		print(ctx)
+		url = "https://viewer.mars.asu.edu/viewer/ctx#P=" + ctx + "&T=2" # Database URL based on CTX
 
-	# Need to load inner HTML code, can only be accessed after webpage fully loads up
-	# Need to open a browser to allow webpage to load file completely
-	browser = webdriver.Firefox() # This won't work if you don't download selenium webdriver
-	browser.get(url)
-	innerHTML = browser.execute_script("return document.body.innerHTML").encode('utf-8')
-
-	# TIFF URL under "Pyramidized TIFF"
-	pyLoc = innerHTML.find('Pyramidized TIFF')
-	bLoc = innerHTML.rfind('href=', 0, pyLoc)
-	eLoc = innerHTML.rfind('" target="_blank">', 0, pyLoc)
-	while(pyLoc==-1 or bLoc==-1 or eLoc==-1): # Error handling, sometimes the page doesn't
-		print("Failed.. retrying")        # fully load before trying to grab inner HTML
-		browser.quit()
-		browser = webdriver.Firefox()
+		# Need to load inner HTML code, can only be accessed after webpage fully loads up
+		# Need to open a browser to allow webpage to load file completely
+		browser = webdriver.Firefox() # This won't work if you don't download selenium webdriver
 		browser.get(url)
 		innerHTML = browser.execute_script("return document.body.innerHTML").encode('utf-8')
+
+		# TIFF URL under "Pyramidized TIFF"
 		pyLoc = innerHTML.find('Pyramidized TIFF')
 		bLoc = innerHTML.rfind('href=', 0, pyLoc)
 		eLoc = innerHTML.rfind('" target="_blank">', 0, pyLoc)
+		while(pyLoc==-1 or bLoc==-1 or eLoc==-1): # Error handling, sometimes the page doesn't
+			print("Failed.. retrying")        # fully load before trying to grab inner HTML
+			browser.quit()
+			browser = webdriver.Firefox()
+			browser.get(url)
+			innerHTML = browser.execute_script("return document.body.innerHTML").encode('utf-8')
+			pyLoc = innerHTML.find('Pyramidized TIFF')
+			bLoc = innerHTML.rfind('href=', 0, pyLoc)
+			eLoc = innerHTML.rfind('" target="_blank">', 0, pyLoc)
 
-	# TIFF URL
-	tiffurl = innerHTML[bLoc+6:eLoc]
-	print(tiffurl)
+		# TIFF URL
+		tiffurl = innerHTML[bLoc+6:eLoc]
+		print(tiffurl)
+		browser.quit()
 
-	# Download TIFF
-	retry=True # Error handling in case of network error
-	while(retry):
-		try:
-			urllib.urlretrieve(tiffurl, ctx+".tiff")
-			retry=False
-		except:
-			retry=True
-			print('Connection error.. Retrying')
-	browser.quit()
-	print(ctx + 'finished.')
+		# Download TIFF
+		retry=True # Error handling in case of network error
+		while(retry):
+			try:
+				urllib.urlretrieve(tiffurl, ctx+".tiff", reporthook=downloadtime)
+				retry=False
+			except (KeyboardInterrupt, SystemExit):
+				raise
+			except Exception as e:
+				print str(e)
+				retry=True
+				print('Connection error.. Retrying')
+		print(ctx + ' finished.')
 
-csvfile.close()
+	csvfile.close()
+
+
+# Function for printing the progress of the TIFF download
+def downloadtime(count, block_size, total_size):
+	global start_time
+	if count == 0:
+		start_time = time.time()
+		return
+	duration = time.time() - start_time
+	progress_size = int(count * block_size)
+	speed = int(progress_size / (1024 * duration))
+	#percent = int(count * block_size * 100 / total_size)
+	percent = min(int(count*block_size*100/total_size),100)
+	sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" % (percent, progress_size / (1024 * 1024), speed, duration))
+	sys.stdout.flush()
+
+
+
+if __name__=='__main__':
+	main()
 
 
 # END SCRIPT
